46117a10f3b5fc70eb3ba805da12c6090fe31f73
seyeong <jang.se.yeong000@gmail.com>
2023-11-16 10:42:08 +0900

    add Cart

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Main from './components/Main';
 import Detail from './components/Detail';
 import About from './components/About';
 import EventPage from './components/EventPage';
+import Cart from './components/Cart';
 
 function App() {
 
@@ -20,12 +21,11 @@ function App() {
     <div className='App'>
       <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="/">Shop</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/cart">Cart</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
@@ -42,6 +42,7 @@ function App() {
           <Route path='one' element={<p>첫 주문 시 양배추즙 서비스</p>}/>
           <Route path='two' element={<p>생일기념 쿠폰받기</p>}/>
         </Route>
+        <Route path='/cart' element={ <Cart /> } />
         <Route path='*' element={<div>없는 페이지입니다</div>} />
       </Routes>
       
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -13,69 +13,81 @@ export default function Detail(){
     // 여기서만 쓰이는 state는 여기서 정의하자
     const [alert, setAlert] = useState(true);
     const [tabId, setTabId] = useState(0);
-    
+    const [fade, setFade] = useState('');
+
     // useEffect 정의
     useEffect(()=>{
-        
+       
         // load, Update 시 실행
         let timer = setTimeout(()=>{
             setAlert(false);
+
         }, 2000);
         
         // 초기화 혹은 unmount 시 실행
         return (()=>{
-            clearTimeout(timer);
+            if(timer) clearTimeout(timer);           
         })
     },[]);
-    console.log('엥')
+
+    useEffect(()=>{
+        let timer = setTimeout(() => {
+            setFade('end');
+        }, 100);
+        return (()=>{
+            if(timer) clearTimeout(timer);
+            setFade('');
+        })
+    },[targetId]);
+
     return (
         <>
-        { alert ? <div className="alert alert-warning">2초 이내 구매 시 할인!!</div> : null}
-        <div className="container">
-                <div className="row">
-                <div className="col-md-6">
-                <img src={`https://codingapple1.github.io/shop/shoes${item.id + 1}.jpg`} width="100%" />
+            <div className={`start ${fade}`}>
+            { alert ? <div className="alert alert-warning">2초 이내 구매 시 할인!!</div> : null}
+            <div className="container">
+                    <div className="row">
+                    <div className="col-md-6">
+                    <img src={`https://codingapple1.github.io/shop/shoes${item.id + 1}.jpg`} width="100%" />
+                    </div>
+                    <div className="col-md-6">
+                    <h4 className="pt-5">{item.title}</h4>
+                    <p>{item.content}</p>
+                    <p>{`${item.price}원`}</p>
+                    <button className="btn btn-danger">주문하기</button> 
+                    </div>
                 </div>
-                <div className="col-md-6">
-                <h4 className="pt-5">{item.title}</h4>
-                <p>{item.content}</p>
-                <p>{`${item.price}원`}</p>
-                <button className="btn btn-danger">주문하기</button> 
-                </div>
-            </div>
-        </div> 
-        {/* <input onChange={(e)=>{setTestVal(e.target.value)}}></input> */}
+            </div> 
+            {/* <input onChange={(e)=>{setTestVal(e.target.value)}}></input> */}
 
-        <Nav variant="tabs"  defaultActiveKey="link0">
-            <Nav.Item>
-                <Nav.Link onClick={()=>{setTabId(0)}} eventKey="link0">버튼0</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={()=>{setTabId(1)}} eventKey="link1">버튼1</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={()=>{setTabId(2)}} eventKey="link2">버튼2</Nav.Link>
-            </Nav.Item>
-        </Nav>
+            <Nav variant="tabs"  defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{setTabId(0)}} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{setTabId(1)}} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{setTabId(2)}} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
 
-        {/* <TabContent selected={tabId}/> */}
+            <TabContent selected={tabId}/>
+        </div>
         </>
     )
 }
 
 function TabContent({selected}){
 
-    //console.log('엥')
     const [fade, setFade] = useState('');
 
     useEffect(()=>{
-
-        setTimeout(() => { setFade('end') }, 100);
-
+        let timer = setTimeout(() => { setFade('end') }, 100);
         return (()=>{
-            setFade('');
+            if (timer) clearTimeout(timer);  
+            setFade('');                   
         });
-    },[fade]);
+    },[selected]);
 
     return (
         <div className= {`start ${fade}`}>
